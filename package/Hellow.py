5a188ce4625d698129d20c6bb3e3d7e809fb7ef1
hongxuli <34845838+hongxuli@users.noreply.github.com>
2019-09-22 00:50:12 -0500

    9.22

---
+++ package/Hellow.py
@@ -0,0 +1,15 @@
+# # # stu1 = {'name':'xiaohong','score':98}
+
+# def print_score(stu):
+#     # print('%s: %s'%(stu['name'],stu['score']))
+#     # print('{}:{}'.format(stu['name'],stu['score']))
+#     print('{}:{}'.format('stu.name','stu.score'))
+
+# # print_score(stu1)
+
+# # def eat():
+# #     print('i can eat')
+
+import test as t
+
+print(t.__name__)
